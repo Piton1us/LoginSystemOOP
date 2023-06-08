a1d2a3bcf29797ea0ecd50552b7845e3c3986606
Piton1us <89169196952@mail.ru>
2023-06-08 22:35:41 +0300

    add styled skeleton

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <link rel="icon" type="png" href="img/0K1yiwxSXX0.jpg">
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="main.css">
+   <title>Login system</title>
+</head>
+
+<body>
+
+   <div class="wrapper">
+
+      <header>
+         <h2>Creative design</h2>
+         <ul>
+            <li>User</li>
+            <li>Button</li>
+         </ul>
+      </header>
+
+      <main>
+         <form action="" method="post">
+            <h1>LOGIN</h1>
+            
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="password" placeholder="Password">
+            <button type="submit" class="btn-form">Sign in</button>
+
+         </form>
+
+         <form action="" method="post">
+            <h1>SIGN UP</h1>
+            <p>Don`t have an account yet? Sign up here!</p>
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="password" placeholder="Password">
+            <input type="password" name="" placeholder="Repeat Password">
+            <input type="email" name="email" placeholder="E-mail">
+            <button type="submit" class="btn-form">Sign in</button>
+
+         </form>
+
+
+      </main>
+
+
+   </div>
+
+</body>
+
+</html>
